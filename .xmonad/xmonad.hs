7fec780356c233a497601ef23f6231d2b3c4fb94
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-09 03:19:24 +0600

    Add files via upload

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,14 +6,27 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 
+import XMonad.Layout.Accordion
+import XMonad.Layout.Grid
 import XMonad.Layout.Magnifier
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
 
 import XMonad.Util.Cursor
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.SpawnOnce
 
+myModMask            = mod4Mask
+myTerminal           = "alacritty"
+myWorkspaces         = ["web", "irc", "code" ] ++ map show [4..9]
+myFocusedBorderColor = "#ff0000"      -- Color of focused border
+myNormalBorderColor  = "#dddddd"      -- Color of inactive border
+myBorderWidth        = 2              -- Width of border around windows
+
+myStartupHook :: X ()
 myStartupHook = do
     spawnOnce "nitrogen --restore &"
     spawnOnce "picom &"
@@ -22,7 +35,9 @@ myStartupHook = do
     setWMName "LG3D"
     setDefaultCursor xC_left_ptr
 
-myLayout = (tiled ||| Mirror tiled ||| Full ||| threeCol)
+
+myLayout = ( tiled ||| Mirror tiled ||| Full ||| Grid ||| spiral(6/7) |||
+             threeCol ||| noBorders (tabbed shrinkText def) ||| Accordion )
   where
     threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
     tiled    = Tall nmaster delta ratio
@@ -30,27 +45,34 @@ myLayout = (tiled ||| Mirror tiled ||| Full ||| threeCol)
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
 
+myManageHook :: ManageHook
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
+    , className =? "MPlayer"   --> doFloat
     ]
 
+myKeys = [ ((myModMask, xK_b), sendMessage ToggleStruts)
+         , ((myModMask .|. shiftMask, xK_z), spawn "slock")
+         , ((myModMask .|. shiftMask, xK_p), spawn "rofi -show run")
+         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+         , ((0, xK_Print), spawn "scrot")
+         ]
+
+main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ ewmh $ docks defaultConfig
-        { manageHook = myManageHook <+> manageHook defaultConfig -- make sure to include myManageHook definition from above
-        , layoutHook = avoidStruts  $  myLayout
+    xmonad $ ewmh $ docks $ def
+        { manageHook = myManageHook <+> manageHook def
+        , layoutHook = smartBorders $ avoidStruts $ myLayout
         , startupHook = myStartupHook
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , terminal = "urxvtc"
-        , workspaces = ["web", "irc", "code" ] ++ map show [4..9]
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
-        , ((mod4Mask .|. shiftMask, xK_p), spawn "rofi -show run")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
+        , modMask = myModMask
+        , focusedBorderColor = myFocusedBorderColor
+        , normalBorderColor = myNormalBorderColor
+        , borderWidth = myBorderWidth
+        , terminal = myTerminal
+        , workspaces = myWorkspaces
+        } `additionalKeys` myKeys
