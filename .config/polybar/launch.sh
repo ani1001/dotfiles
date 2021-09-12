6dbda843b8a42819ac7cf5b1b712111c8f3bc903
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-12 22:36:58 +0600

    Add files via upload

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
