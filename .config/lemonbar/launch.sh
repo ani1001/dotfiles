6340bc16436e5c719c8214f9280a5637b9082f6c
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-17 02:00:18 +0600

    Add files via upload

--- .config/lemonbar/launch.sh
+++ .config/lemonbar/launch.sh
@@ -3,5 +3,5 @@
 killall -q lemonbar
 killall -q citrus
 
-$HOME/.config/lemonbar/citrus | lemonbar -p -f '-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*' -u 2 \
-	-F "#d8dee9" -B "#2e3440" -U "#ebcb8b" | $SHELL
+$HOME/.config/lemonbar/citrus | lemonbar -p -F "#d8dee9" -B "#2e3440" -U "#ebcb8b" -u 2 \
+	-f '-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*' | $SHELL
