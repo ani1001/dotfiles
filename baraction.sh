9d68a8654b8489da12a7cff87c40d8e1fd4fb1e4
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-16 09:34:21 +0600

    Add files via upload

--- baraction.sh
+++ baraction.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Example Bar Action Script for OpenBSD-current.
+# Example Bar Action Script for Linux.
+# Requires: acpi, iostat.
+# Tested on: Debian 10, Fedora 31.
 #
 
 print_date() {
@@ -12,65 +14,63 @@ print_date() {
 }
 
 print_mem() {
-	MEM=`/usr/bin/top | grep Free: | cut -d " " -f6`
-	echo -n "Free mem: $MEM  "
+	MEM=`/usr/bin/free -m | grep ^Mem: | sed -E 's/ +/ /g' | cut -d ' ' -f4`
+	echo -n "Free mem: ${MEM}M  "
 }
 
 _print_cpu() {
-	printf "CPU: %3d%% User %3d%% Nice %3d%% Sys %3d%% Spin %3d%% Int %3d%% Idle  " $1 $2 $3 $4 $5 $6
+	printf "CPU: %3d%% User %3d%% Nice %3d%% Sys %3d%% Idle  " $1 $2 $3 $6
 }
 
 print_cpu() {
 	OUT=""
-	# iostat prints each column justified to 3 chars, so if one counter
-	# is 100, it jams up agains the preceeding one. sort this out.
+	# Remove the decimal part from all the percentages.
 	while [ "${1}x" != "x" ]; do
-		if [ ${1} -gt 99 ]; then
-			OUT="$OUT ${1%100} 100"
-		else
-			OUT="$OUT ${1}"
-		fi
+		OUT="$OUT `echo "${1}" | cut -d '.' -f1`"
 		shift;
 	done
 	_print_cpu $OUT
 }
 
 print_cpuspeed() {
-	CPU_SPEED=`/sbin/sysctl hw.cpuspeed | cut -d "=" -f2`
+	CPU_SPEED=`/usr/bin/lscpu | grep '^CPU MHz:' | sed -E 's/ +/ /g' | cut -d ' ' -f3 | cut -d '.' -f1`
 	printf "CPU speed: %4d MHz  " $CPU_SPEED
 }
 
 print_bat() {
-	BAT_STATUS=$1
-	BAT_LEVEL=$2
-	AC_STATUS=$3
+	AC_STATUS="$3"
+	BAT_STATUS="$6"
+	# Most battery statuses fit into a single word, except "Not charging"
+	# for which we need to have special handling.
+	if [ "$BAT_STATUS" = "Not" ]; then
+		BAT_STATUS="$BAT_STATUS $7"
+		shift
+	fi
+	BAT_LEVEL="`echo "$7" | tr -d ','`"
 
-	if [ $AC_STATUS -ne 255 -o $BAT_STATUS -lt 4 ]; then
-		if [ $AC_STATUS -eq 0 ]; then
-			echo -n "on battery (${BAT_LEVEL}%)"
+	if [ "$AC_STATUS" != "" -o "$BAT_STATUS" != "" ]; then
+		if [ "$BAT_STATUS" = "Discharging," ]; then
+			echo -n "on battery ($BAT_LEVEL)"
 		else
-			case $AC_STATUS in
-			1)
+			case "$AC_STATUS" in
+			on-line)
 				AC_STRING="on AC: "
-				;;
-			2)
-				AC_STRING="on backup AC: "
 				;;
 			*)
 				AC_STRING=""
 				;;
-			esac;
-			case $BAT_STATUS in
-			4)
+			esac
+			case "$BAT_STATUS" in
+			"")
 				BAT_STRING="(no battery)"
 				;;
-			[0-3])
-		 		BAT_STRING="(battery ${BAT_LEVEL}%)"
+			*harging,|Full,)
+				BAT_STRING="(battery $BAT_LEVEL)"
 				;;
 			*)
 				BAT_STRING="(battery unknown)"
 				;;
-			esac;
+			esac
 
 			FULL="${AC_STRING}${BAT_STRING}"
 			if [ "$FULL" != "" ]; then
@@ -80,19 +80,19 @@ print_bat() {
 	fi
 }
 
-# cache the output of apm(8), no need to call that every second.
-APM_DATA=""
+# Cache the output of acpi(8), no need to call that every second.
+ACPI_DATA=""
 I=0
 while :; do
-	IOSTAT_DATA=`/usr/sbin/iostat -C -c 2 | tail -n 1 | grep '[0-9]$'`
+	IOSTAT_DATA=`/usr/bin/iostat -c | grep '[0-9]$'`
 	if [ $I -eq 0 ]; then
-		APM_DATA=`/usr/sbin/apm -alb`
+		ACPI_DATA=`/usr/bin/acpi -a 2>/dev/null; /usr/bin/acpi -b 2>/dev/null`
 	fi
 	# print_date
 	print_mem
-	print_cpu $IOSTAT_DATA
+	# print_cpu $IOSTAT_DATA
 	print_cpuspeed
-	print_bat $APM_DATA
+	# print_bat $ACPI_DATA
 	echo ""
 	I=$(( ( ${I} + 1 ) % 11 ))
 	sleep 1
