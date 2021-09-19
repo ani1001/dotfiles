f91efee0394e603a2fc4f9ec708064e30df39cde
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-20 02:02:06 +0600

    Add files via upload

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,18 @@
-(cua-mode t)
 (setq make-backup-files nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes '(tango-dark))
+ '(custom-safe-themes
+   '("4e7e04c4b161dd04dc671fb5288e3cc772d9086345cb03b7f5ed8538905e8e27" "fb09acc5f09e521581487697c75b71414830b1b0a2405c16a9ece41b2ae64222" default))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "ShureTechMono NF" :foundry "UKWN" :slant normal :weight normal :height 113 :width normal)))))
