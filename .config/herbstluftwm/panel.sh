d3fe9efb24f458f12f11fbc116ee8ca12d2d2f09
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-20 21:14:33 +0600

    Add files via upload

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -19,9 +19,8 @@ y=${geometry[1]}
 panel_width=${geometry[2]}
 panel_height=16
 font="-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*"
-# extract colors from hlwm and omit alpha-value
-bgcolor=$(hc get frame_border_normal_color|sed 's,^\(\#[0-9a-f]\{6\}\)[0-9a-f]\{2\}$,\1,')
-selbg=$(hc get window_border_active_color|sed 's,^\(\#[0-9a-f]\{6\}\)[0-9a-f]\{2\}$,\1,')
+bgcolor=$(hc get frame_border_normal_color)
+selbg=$(hc get window_border_active_color)
 selfg='#101010'
 
 ####
