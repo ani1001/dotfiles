92a31d37e330acba1a8bde61bce28a6c314cfd2d
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-28 23:56:01 +0600

    Delete .config/lemonbar directory

--- .config/lemonbar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-killall -q lemonbar
-killall -q $HOME/.config/lemonbar/citrus
-
-$HOME/.config/lemonbar/citrus | lemonbar -p -F "#d8dee9" -B "#2e3440" -U "#ebcb8b" -u 2 \
-	-f '-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*' | $SHELL
