c2159c20117e228919da2f8669cf71c3bf9f5296
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-30 01:03:31 +0600

    Add files via upload

--- .config/qtile/backup/garuda-qtile/scripts/autostart.sh
+++ .config/qtile/backup/garuda-qtile/scripts/autostart.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#starting utility applications at boot time
+#run nm-applet &
+picom --config $HOME/.config/picom/picom.conf &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+run lxpolkit
+#run volumeicon &
+#nitrogen --random --set-zoom-fill &
+run nitrogen --restore
