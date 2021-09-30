498cb4b9cad1729e14232811383d06ecd9e1923b
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-30 13:26:09 +0600

    Add files via upload

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -199,6 +199,12 @@ widget_defaults = init_widgets_defaults()
 def init_widgets_list():
     prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
     widgets_list = [
+        widget.Sep(
+            linewidth = 1,
+            padding = 10,
+            foreground = colors[2],
+            background = colors[1]
+            ),
         widget.Image(
             filename = "~/.config/qtile/icons/python.png",
             iconsize = 10,
@@ -212,10 +218,10 @@ def init_widgets_list():
             background = colors[1]
             ),
         widget.GroupBox(
-            active=colors[9],
-            inactive=colors[5],
-            highlight_method='line',
-            this_current_screen_border=colors[3],
+            active = colors[9],
+            inactive = colors[5],
+            highlight_method = 'line',
+            this_current_screen_border = colors[3],
             foreground = colors[2],
             background = colors[1]
             ),
@@ -239,15 +245,16 @@ def init_widgets_list():
         #    background = colors[1]
         #    ),
         #widget.Systray(
-        #    background=colors[1],
-        #    icon_size=20,
+        #    background = colors[1],
+        #    icon_size = 20,
         #    padding = 4
         #    ),
         widget.Sep(
             linewidth = 1,
             padding = 10,
             foreground = colors[2],
-            background = colors[1]),
+            background = colors[1]
+            ),
         widget.CPU(
             font="Noto Sans",
             format="CPU {freq_current}GHz {load_percent}%",
@@ -260,7 +267,7 @@ def init_widgets_list():
         #    border_color = colors[2],
         #    fill_color = colors[3],
         #    graph_color = colors[3],
-        #    background=colors[1],
+        #    background = colors[1],
         #    border_width = 1,
         #    line_width = 1,
         #    core = "all",
@@ -287,21 +294,21 @@ def init_widgets_list():
             background = colors[1]
             ),
         widget.Net(
-            font="Noto Sans",
-            fontsize=12,
-            interface="all",
-            foreground=colors[2],
-            background=colors[1],
+            font = "Noto Sans",
+            fontsize = 12,
+            interface = "all",
+            foreground = colors[2],
+            background = colors[1],
             padding = 0,
             ),
         #widget.NetGraph(
-        #    font="Noto Sans",
-        #    fontsize=12,
-        #    bandwidth="down",
-        #    interface="auto",
+        #    font = "Noto Sans",
+        #    fontsize = 12,
+        #    bandwidth = "down",
+        #    interface = "auto",
         #    fill_color = colors[3],
-        #    foreground=colors[2],
-        #    background=colors[1],
+        #    foreground = colors[2],
+        #    background = colors[1],
         #    graph_color = colors[3],
         #    border_color = colors[2],
         #    padding = 0,
@@ -318,7 +325,7 @@ def init_widgets_list():
             foreground = colors[5],
             background = colors[1],
             fontsize = 12,
-            format="%Y-%m-%d %H:%M"
+            format = "%Y-%m-%d %H:%M"
             ),
         widget.Sep(
             linewidth = 1,
@@ -339,10 +346,16 @@ def init_widgets_list():
             ),
         widget.CurrentLayoutIcon(
             custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
-            foreground = colors[8],
+            foreground = colors[5],
             background = colors[1],
             padding = 0,
             scale = 0.7
+            ),
+        widget.Sep(
+            linewidth = 1,
+            padding = 10,
+            foreground = colors[2],
+            background = colors[1]
             ),
         ]
     return widgets_list
@@ -356,7 +369,7 @@ def init_widgets_screen():
 widgets_screen = init_widgets_screen()
 
 def init_screens():
-    return [Screen(top=bar.Bar(widgets=init_widgets_screen(), size=24, opacity=0.8))]
+    return [Screen(top=bar.Bar(widgets=init_widgets_screen(), size=20, opacity=0.9))]
 screens = init_screens()
 
 # Drag floating layouts.
