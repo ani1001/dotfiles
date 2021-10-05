9a1720b0b295958071da0a9e385669bd8e36d472
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-10-05 14:58:35 +0600

    Add files via upload

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -36,7 +36,7 @@ import XMonad.Util.SpawnOnce
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "st"
+myTerminal      = "alacritty"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
