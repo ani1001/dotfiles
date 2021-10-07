de3fc6cb22f9ffd876212a669ab55b5dde4c12d9
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-10-08 01:44:02 +0600

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -96,8 +96,8 @@ end
 ## Useful aliases
 # Replace ls with exa
 alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias la='exa -a  --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l  --color=always --group-directories-first --icons'  # long format
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
 alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
 
@@ -106,9 +106,9 @@ alias l.="exa -a | egrep '^\.'"                                     # show only 
 #[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
 
 # Common use
-#alias grubup="sudo update-grub"
+alias grubup="sudo update-grub"
 #alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-#alias tarnow='tar -acf '
+alias tarnow='tar -acf '
 alias untar='tar -zxvf '
 alias wget='wget -c '
 #alias rmpkg="sudo pacman -Rdd"
@@ -120,17 +120,17 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
-#alias dir='dir --color=auto'
-#alias vdir='vdir --color=auto'
-#alias grep='grep --color=auto'
-#alias fgrep='fgrep --color=auto'
-#alias egrep='egrep --color=auto'
-#alias hw='hwinfo --short'                                   # Hardware Info
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
 #alias big="expac -H M '%m\t%n' | sort -h | nl"             # Sort installed packages according to size in MB
 #alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'	    # List amount of -git packages
 
 # Get fastest mirrors
-#alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+#alias mirror="sudo reflector  -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
 #alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
 #alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
 #alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
@@ -152,9 +152,9 @@ alias ......='cd ../../../../..'
 
 
 ## Run pfetch if session is interactive
-if status --is-interactive
-   pfetch
-end
+#if status --is-interactive
+#   pfetch
+#end
 
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
