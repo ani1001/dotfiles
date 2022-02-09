4137c0396e8f2b940033dcf50066eb6fba92d338
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2022-02-09 16:32:13 +0600

    Add files via upload

---
+++ .config/qtile/modules/hooks.py
@@ -0,0 +1,34 @@
+from libqtile import hook
+import subprocess
+import os
+import shutil
+import re
+
+home = os.path.expanduser('~')
+
+@hook.subscribe.restart
+def cleanup():
+    shutil.rmtree(os.path.expanduser('~/.config/qtile/__pycache__'))
+
+@hook.subscribe.shutdown
+def killall():
+    shutil.rmtree(os.path.expanduser('~/.config/qtile/__pycache__'))
+    subprocess.Popen(['killall', 'urxvtd', 'lxpolkit', 'nitrogen', 'picom'])
+
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
+
+@hook.subscribe.startup
+def start_always():
+    # Set the cursor to something sane in X
+    subprocess.Popen(['xsetroot', '-cursor_name', 'left_ptr'])
+
+@hook.subscribe.client_new
+def set_floating(window):
+    if (window.window.get_wm_transient_for()
+            or window.window.get_wm_type() in floating_types):
+        window.floating = True
+
+floating_types = ["notification", "toolbar", "splash", "dialog"]
