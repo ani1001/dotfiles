38deafbb2e61bc082d29b797ffff9ceb2b1e1228
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2022-02-10 10:40:30 +0600

    Add files via upload

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -6,6 +6,9 @@ from modules.hooks import *
 from modules.widgets import colors, prompt, widget_defaults
 from modules.screens import screens
 
+import os
+
+main = None  # WARNING: this is deprecated and will be removed soon
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
@@ -15,4 +18,4 @@ auto_fullscreen = True
 focus_on_window_activation = "focus"
 reconfigure_screens = True
 auto_minimize = True
-wmname = "LG3D"
+wmname = "Qtile"
